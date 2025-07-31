02b733e9d9f454e593ebd2e76e602c42354c61e3
Pratyay <Pratyay@users.noreply.github.com>
2025-07-31 23:21:22 +0530

    Update DataBuilderMetadataManager.java
    Testing agent

--- src/main/java/com/flipkart/databuilderframework/engine/DataBuilderMetadataManager.java
+++ src/main/java/com/flipkart/databuilderframework/engine/DataBuilderMetadataManager.java
@@ -99,7 +99,7 @@ public class DataBuilderMetadataManager {
     
     public DataBuilderMetadataManager register(Set<String> consumes, Set<String> optionals, Set<String> access, String produces,
             String builder, Class<? extends DataBuilder> dataBuilder) throws DataBuilderFrameworkException {
-        DataBuilderMeta metadata = new DataBuilderMeta(consumes, produces, builder, optionals, access);
+        DataBuilderMeta metadata = null;
         if(meta.containsKey(builder)) {
             throw new DataBuilderFrameworkException(DataBuilderFrameworkException.ErrorCode.BUILDER_EXISTS,
                     "A builder with name " + builder + " already exists");
