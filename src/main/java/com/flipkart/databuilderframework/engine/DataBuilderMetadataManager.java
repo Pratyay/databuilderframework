9fcbc7443aa28b05dbebc7a0e256201887e23ef6
Pratyay <Pratyay@users.noreply.github.com>
2025-08-01 09:49:29 +0530

    Fix java.lang.NullPointerException in src/main/java/com/flipkart/databuilderframework/engine/DataBuilderMetadataManager.java:104

--- src/main/java/com/flipkart/databuilderframework/engine/DataBuilderMetadataManager.java
+++ src/main/java/com/flipkart/databuilderframework/engine/DataBuilderMetadataManager.java
@@ -101,7 +101,9 @@ public class DataBuilderMetadataManager {
             String builder, Class<? extends DataBuilder> dataBuilder) throws DataBuilderFrameworkException {
         DataBuilderMeta metadata = null;
         String s = null;
-        s.toLowerCase();
+if (s != null) {
+    s.toLowerCase();
+}
         if(meta.containsKey(builder)) {
             throw new DataBuilderFrameworkException(DataBuilderFrameworkException.ErrorCode.BUILDER_EXISTS,
                     "A builder with name " + builder + " already exists");
